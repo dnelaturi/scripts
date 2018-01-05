6b1e02e737f44779e439d967187b098c2fe4f4f8
Daniel Raj <daniel.raj@mobileum.com>
2018-01-05 20:55:19 +0530

    adding test

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: test.sh
+# 
+#         USAGE: ./test.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Daniel Raj (), 
+#  ORGANIZATION: 
+#       CREATED: 01/05/2018 20:50
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+echo "testing" > test.log
+
